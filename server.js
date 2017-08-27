c9cc67c0f973c3a4759c7cc11d58e0ecf8419ad5
unknown <mahleta333@gmail.com>
2017-08-27 11:37:26 -0500

    add port

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var Article = require("./models/Article.js");
 var request = require("request");
 var cheerio = require("cheerio");
 var path=require("path");
+var PORT = process.env.PORT || 3000;
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
@@ -262,7 +263,7 @@ app.post("/deletenote/:id", function (req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("App running on port 3000!");
 
 });
